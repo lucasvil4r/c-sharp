2b81782a362c2e2cf6ea65f4f99bc94bf65f4773
lucasvil4r <lucasvilar-celestino@hotmail.com>
2023-02-07 10:56:50 -0300

    Videoaula - Parte 3

--- c-sharp/Program.cs
+++ c-sharp/Program.cs
@@ -8,6 +8,15 @@ namespace PrimeiroProjeto
 {
     class Program
     {
+        static void Main(string[] args)
+        {
+            NewMethod();
 
+        }
+
+        private static void NewMethod()
+        {
+            Console.Write("Olá Mundo");
+        }
     }
 }
