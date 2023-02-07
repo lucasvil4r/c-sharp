3fc97b456d445a0470d62c67e2bde4ca895564a7
lucasvil4r <lucasvilar-celestino@hotmail.com>
2023-02-07 15:44:38 -0300

    Organizando aulas por projetos

---
+++ Aula 1 - Introdução ao Visual Studio e C-Sharp/ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+
+// See https://aka.ms/new-console-template for more information
+namespace PrimeiroProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NewMethod();
+        }
+
+        private static void NewMethod()
+        {
+            Console.WriteLine("My first program");
+            Console.WriteLine("That is a application Console");
+        }
+    }
+}
