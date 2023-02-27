144a3b77625dfea5f628deff7eefafe7f5df2af2
lucasvil4r <lucasvilar-celestino@hotmail.com>
2023-02-27 15:46:24 -0300

    Videoaula - Parte 2

---
+++ Aula 3 - Instruções, tipos de dados, variáveis e operadores/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Aula03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Texto
+            String nome = "Teste";
+
+            //Numero Inteiro
+            short idade = 10;
+            int id = 10;
+            long numeroLong = 10000;
+
+            //Numero com fração
+            double porcentagem = 4.58889189489;
+            decimal salario = Convert.ToDecimal(5645.50);
+            float comissao = Convert.ToSingle(3.5);
+
+            //Data
+            DateTime data = new DateTime();
+            DateTime agora = DateTime.Now;
+
+            //Boolean
+            bool = true;
+        }
+    }
+}
