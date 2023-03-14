a45ce4f248451d053c3af17d9d9619a79be65ac8
lucasvil4r <lucasvilar-celestino@hotmail.com>
2023-03-14 17:12:42 -0300

    Aula 5 - Estruturas de repetição

---
+++ Aula 5 - Estruturas de repetição/Aula 5 - Estruturas de repetição/ConsoleApp/Program.cs
@@ -0,0 +1,36 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        RepetitionFor();
+        RepetitionWhile();
+        RepetitionDoWhile();
+    }
+
+    static void RepetitionFor()
+    {
+        for (int x = 1 ; x <= 10 ; x++ )
+        {
+            Console.WriteLine( x );
+        }
+    }
+    static void RepetitionWhile()
+    {
+        int x = 1 ;
+        while ( x <= 10 )
+        {
+            Console.WriteLine( x );
+            x++;
+        }
+    }
+    static void RepetitionDoWhile()
+    {
+        int x = 1;
+        do
+        {
+            Console.WriteLine(x);
+            x++;
+        }
+        while (x <= 10);
+    }
+}
