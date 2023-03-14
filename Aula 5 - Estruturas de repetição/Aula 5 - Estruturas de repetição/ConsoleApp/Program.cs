865f9d00cbfa078650b12d88b94fc59f50086e97
lucasvil4r <lucasvilar-celestino@hotmail.com>
2023-03-14 17:50:49 -0300

    Aula 5 - Estruturas de repetição

--- Aula 5 - Estruturas de repetição/Aula 5 - Estruturas de repetição/ConsoleApp/Program.cs
+++ Aula 5 - Estruturas de repetição/Aula 5 - Estruturas de repetição/ConsoleApp/Program.cs
@@ -1,10 +1,16 @@
-﻿internal class Program
+﻿using System.Reflection.Emit;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        RepetitionFor();
-        RepetitionWhile();
-        RepetitionDoWhile();
+        //RepetitionFor();
+        //RepetitionWhile();
+        //RepetitionDoWhile();
+        //Exercicios01();
+        //Exercicios03();
+        //Exercicios04();
+        Exercicios05();
     }
 
     static void RepetitionFor()
@@ -32,5 +38,57 @@
             x++;
         }
         while (x <= 10);
+    }
+    static void Exercicios01()
+    {
+        int n = 0;
+        string label1 = "";
+
+        while (n <= 10)
+        {
+            label1 += n.ToString("00");
+            n += 2;
+        }
+        Console.WriteLine(label1);
+    }
+
+    static void Exercicios03()
+    {
+        int n = 12;
+        string label1 = "";
+
+        while (n <= 10)
+        {
+            n -= 2;
+            label1 += n.ToString("00");
+        }
+        Console.WriteLine(label1);
+    }
+
+    static void Exercicios04()
+    {
+        int n = 12;
+        string label1 = "";
+
+        for (n = 0; n <= 10; n += 2)
+        {
+            label1 += n.ToString("00");
+
+        }
+        Console.WriteLine(label1);
+    }
+
+    static void Exercicios05()
+    {
+        int n = 0;
+        string label1 = "";
+
+        do
+        {
+            n += 2;
+            label1 += n.ToString("00");
+        } while (n >= 10);
+
+        Console.WriteLine(label1);
     }
 }
