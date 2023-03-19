f1f1838692149577e09b3cf463a8e8016dcf335b
lucasvil4r <lucasvilar-celestino@hotmail.com>
2023-03-19 11:49:20 -0300

    Aula 6 - Tratamento de erros

---
+++ Aula 6 - Tratamento de erros/Aula 6 - Tratamento de erros/ConsoleApp2/Program.cs
@@ -0,0 +1,40 @@
+﻿
+using System.Reflection.Emit;
+using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Debug();
+    }
+
+    static void Debug()
+    {
+        try
+        {
+            decimal valorProduto = Convert.ToDecimal(Console.ReadLine());
+            decimal quantidade = Convert.ToInt32(Console.ReadLine());
+            decimal total = valorProduto + quantidade;
+
+            Console.WriteLine(total);
+        }
+        //Somente erros de formatos ele entrará nesse catch
+        catch (FormatException)
+        {
+            Console.WriteLine("Formato de dados de entrada invalido, favor verificar!");
+        }
+        //Somente erros de formatos ele entrará nesse catch
+        catch (OverflowException)
+        {
+            Console.WriteLine("Numero muito grande, favor verificar!");
+        }
+        //Variavel que é instanciada pelo SO automaticamente e retorna erro
+        catch (Exception ex)
+        {
+            Console.WriteLine("Erro generico");
+        }
+        // Finally é executado sempre com erros ou sem erros 
+        finally { Console.WriteLine("Fim do programa"); }
+    }
+}
