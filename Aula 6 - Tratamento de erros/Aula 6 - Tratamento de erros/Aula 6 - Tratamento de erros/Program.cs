324c16ba9a12f3636f29ab35c3b6e6fc68a8325e
lucasvil4r <lucasvilar-celestino@hotmail.com>
2023-03-19 11:34:01 -0300

    Aula 6 - Tratamento de erros

--- Aula 6 - Tratamento de erros/Aula 6 - Tratamento de erros/Aula 6 - Tratamento de erros/Program.cs
+++ Aula 6 - Tratamento de erros/Aula 6 - Tratamento de erros/Aula 6 - Tratamento de erros/Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System.Reflection.Emit;
+using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Exception01();
+        Exception02();
+    }
+
+    static void Exception01()
+    {
+        try 
+        {
+            decimal valorProduto = Convert.ToDecimal(Console.ReadLine());
+            decimal quantidade = Convert.ToInt32 (Console.ReadLine());
+            decimal total = valorProduto + quantidade;
+
+            Console.WriteLine(total);
+        }
+        //Variavel que é instanciada pelo SO automaticamente e retorna erro
+        // catch generico
+        catch
+        {
+            Console.WriteLine("Erro na entrada de dados, favor verificar");
+        }
+    }
+    static void Exception02()
+    {
+        try
+        {
+            decimal valorProduto = Convert.ToDecimal(Console.ReadLine());
+            decimal quantidade = Convert.ToInt32(Console.ReadLine());
+            decimal total = valorProduto + quantidade;
+
+            Console.WriteLine(total);
+        }
+        //Somente erros de formatos ele entrará nesse catch
+        catch (FormatException)
+        {
+            Console.WriteLine("Formato de dados de entrada invalido, favor verificar!");
+        }
+        //Somente erros de formatos ele entrará nesse catch
+        catch (OverflowException)
+        {
+            Console.WriteLine("Numero muito grande, favor verificar!");
+        }
+        //Variavel que é instanciada pelo SO automaticamente e retorna erro
+        catch (Exception ex)
+        {
+            Console.WriteLine("Erro generico");
+        }
+    }
+}
