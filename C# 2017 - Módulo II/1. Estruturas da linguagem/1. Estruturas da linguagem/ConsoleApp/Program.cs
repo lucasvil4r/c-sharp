7cabc754bf41638a72e37b0745e30da45981dca7
lucasvil4r <lucasvilar-celestino@hotmail.com>
2023-03-22 10:22:05 -0300

    1. Estruturas da linguagem

---
+++ C# 2017 - Módulo II/1. Estruturas da linguagem/1. Estruturas da linguagem/ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace estruturasLinguagem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ultilizando classe System do core do windows "dll"
+            Console.WriteLine();
+
+            //Ultizando uma struct
+            int x = 10;
+
+            // Ultilizando enum
+            DayOfWeek dia = DateTime.Now.DayOfWeek;
+        }
+    }
+}
