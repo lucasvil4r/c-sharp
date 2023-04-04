046c2a0f48b72e52b5eec941670f6ccfeb1afec9
lucasvil4r <lucasvilar-celestino@hotmail.com>
2023-04-04 11:26:36 -0300

    2. Definindo o projeto do curso

---
+++ C# 2017 - Módulo II/LojaNet/Pedidos.cs
@@ -0,0 +1,26 @@
+﻿using LojaNet.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LojaNet.Models
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+        public DateTime Data { get; set; }
+        public Cliente Cliente { get; set; }
+        public List<Item> Items { get; set; }
+        public FormaPagamentoEnum FormaPagamento { get; set; }
+
+        public class Item
+        {
+            public int Ordem { get; set; }
+            public Produto Produto { get; set; }
+            public int Quantidade { get; set; }
+            public decimal Preco { get; set; }
+        }
+    }
+}
