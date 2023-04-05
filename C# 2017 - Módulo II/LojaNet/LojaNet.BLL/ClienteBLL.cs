7c62211d5146a5545e741a8ef510d2b1be18e0f8
lucasvil4r <lucasvilar-celestino@hotmail.com>
2023-04-05 13:53:25 -0300

    Camada de regra de negócio

---
+++ C# 2017 - Módulo II/LojaNet/LojaNet.BLL/ClienteBLL.cs
@@ -0,0 +1,50 @@
+﻿using LojaNet.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LojaNet.BLL
+{
+    public class ClienteBLL : InterfaceClienteDados
+    {
+        public void Change(Cliente cliente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Cliente cliente)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Cliente> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Cliente GetByEmail(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Cliente GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Set(Cliente cliente)
+        {
+            if (string.IsNullOrEmpty(cliente.Nome))
+            {
+                throw new ApplicationException("O nome deve ser informado");
+            }
+
+            if (string.IsNullOrEmpty(cliente.Id))
+            {
+                throw new ApplicationException("O nome deve ser informado");
+            }
+        }
+    }
+}
