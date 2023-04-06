7b318754ad23eae438a7aa0861ae3d2c0f35ae14
lucasvil4r <lucasvilar-celestino@hotmail.com>
2023-04-06 17:17:42 -0300

    5. Criando a interface do usuário

---
+++ C# 2017 - Módulo II/LojaNet/LojaNet.UI.Web/Controllers/ClienteController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LojaNet.UI.Web.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
