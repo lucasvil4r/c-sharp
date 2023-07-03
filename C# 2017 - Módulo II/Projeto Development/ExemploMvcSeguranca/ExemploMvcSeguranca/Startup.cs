fcb160d93732e9a5337ebc02c8e4c3dfca5b2720
lucasvil4r <lucasvilar-celestino@hotmail.com>
2023-07-03 18:46:57 -0300

    Segurança

---
+++ C# 2017 - Módulo II/Projeto Development/ExemploMvcSeguranca/ExemploMvcSeguranca/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ExemploMvcSeguranca.Startup))]
+namespace ExemploMvcSeguranca
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
