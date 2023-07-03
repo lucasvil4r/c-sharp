ed56b8092f7c46ee0c469f3d16cda25946d60195
lucasvil4r <lucasvilar-celestino@hotmail.com>
2023-07-03 19:57:47 -0300

    Entity Framework

---
+++ C# 2017 - Módulo II/Projeto Development/LojaNet/LojaNet.UI.Web/Models/NovoUsuarioViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LojaNet.UI.Web.Models
+{
+    public class NovoUsuarioViewModel
+    {
+        public string Nome { get; set; }
+        public string Senha { get; set; }
+        public string ConfirmarSenha { get; set; }
+
+    }
+}
